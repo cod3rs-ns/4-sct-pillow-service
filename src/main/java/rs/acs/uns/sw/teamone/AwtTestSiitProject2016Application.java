628f34744544d7145d26c4833b821146f9ba0107
Dragutin Marjanovic <dmarjanovic94@gmail.com>
2016-10-16 22:12:15 +0200

    Initial spring-boot project structure

---
+++ src/main/java/rs/acs/uns/sw/teamone/AwtTestSiitProject2016Application.java
@@ -0,0 +1,12 @@
+package rs.acs.uns.sw.teamone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwtTestSiitProject2016Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwtTestSiitProject2016Application.class, args);
+	}
+}
---
+++ src/test/java/rs/acs/uns/sw/teamone/AwtTestSiitProject2016ApplicationTests.java
@@ -0,0 +1,16 @@
+package rs.acs.uns.sw.teamone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AwtTestSiitProject2016ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
