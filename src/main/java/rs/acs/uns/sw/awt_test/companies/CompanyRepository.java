1a4658eb71afb56cf9dd21620729541dd7881fc1
Keky <st.keky@gmail.com>
2016-10-31 16:53:33 +0100

    Mark REST API

--- src/main/java/rs/acs/uns/sw/awt_test/companies/CompanyRepository.java
+++ src/main/java/rs/acs/uns/sw/awt_test/companies/CompanyRepository.java
@@ -1,9 +1,6 @@
 package rs.acs.uns.sw.awt_test.companies;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 /**
  * Spring Data JPA repository for the Company entity.
---
+++ src/main/java/rs/acs/uns/sw/awt_test/marks/MarkController.java
@@ -0,0 +1,119 @@
+package rs.acs.uns.sw.awt_test.marks;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rs.acs.uns.sw.awt_test.util.HeaderUtil;
+import rs.acs.uns.sw.awt_test.util.PaginationUtil;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * REST controller for managing Mark.
+ */
+@RestController
+@RequestMapping("/api")
+public class MarkController {
+
+    private final Logger log = LoggerFactory.getLogger(MarkService.class);
+
+    @Autowired
+    private MarkService markService;
+
+    /**
+     * POST  /marks : Create a new mark.
+     *
+     * @param mark the mark to create
+     * @return the ResponseEntity with status 201 (Created) and with body the new mark, or with status 400 (Bad Request) if the mark has already an ID
+     * @throws URISyntaxException if the Location URI syntax is incorrect
+     */
+    @PostMapping("/marks")
+    public ResponseEntity<Mark> createMark(@Valid @RequestBody Mark mark) throws URISyntaxException {
+        log.debug("REST request to save Mark : {}", mark);
+        if (mark.getId() != null) {
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("mark", "idexists", "A new mark cannot already have an ID")).body(null);
+        }
+        Mark result = markService.save(mark);
+        return ResponseEntity.created(new URI("/api/marks/" + result.getId()))
+                .headers(HeaderUtil.createEntityCreationAlert("mark", result.getId().toString()))
+                .body(result);
+    }
+
+    /**
+     * PUT  /marks : Updates an existing mark.
+     *
+     * @param mark the mark to update
+     * @return the ResponseEntity with status 200 (OK) and with body the updated mark,
+     * or with status 400 (Bad Request) if the mark is not valid,
+     * or with status 500 (Internal Server Error) if the mark couldnt be updated
+     * @throws URISyntaxException if the Location URI syntax is incorrect
+     */
+    @PutMapping("/marks")
+    public ResponseEntity<Mark> updateMark(@Valid @RequestBody Mark mark) throws URISyntaxException {
+        log.debug("REST request to update Mark : {}", mark);
+        if (mark.getId() == null) {
+            return createMark(mark);
+        }
+        Mark result = markService.save(mark);
+        return ResponseEntity.ok()
+                .headers(HeaderUtil.createEntityUpdateAlert("mark", mark.getId().toString()))
+                .body(result);
+    }
+
+    /**
+     * GET  /marks : get all the marks.
+     *
+     * @param pageable the pagination information
+     * @return the ResponseEntity with status 200 (OK) and the list of marks in body
+     * @throws URISyntaxException if there is an error to generate the pagination HTTP headers
+     */
+    @GetMapping("/marks")
+    public ResponseEntity<List<Mark>> getAllMarks(Pageable pageable)
+            throws URISyntaxException {
+        log.debug("REST request to get a page of Marks");
+        Page<Mark> page = markService.findAll(pageable);
+        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/marks");
+        return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
+    }
+
+    /**
+     * GET  /marks/:id : get the "id" mark.
+     *
+     * @param id the id of the mark to retrieve
+     * @return the ResponseEntity with status 200 (OK) and with body the mark, or with status 404 (Not Found)
+     */
+    @GetMapping("/marks/{id}")
+    public ResponseEntity<Mark> getMark(@PathVariable Long id) {
+        log.debug("REST request to get Mark : {}", id);
+        Mark mark = markService.findOne(id);
+        return Optional.ofNullable(mark)
+                .map(result -> new ResponseEntity<>(
+                        result,
+                        HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    /**
+     * DELETE  /marks/:id : delete the "id" mark.
+     *
+     * @param id the id of the mark to delete
+     * @return the ResponseEntity with status 200 (OK)
+     */
+    @DeleteMapping("/marks/{id}")
+    public ResponseEntity<Void> deleteMark(@PathVariable Long id) {
+        log.debug("REST request to delete Mark : {}", id);
+        markService.delete(id);
+        return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert("mark", id.toString())).build();
+    }
+
+}
