294513462cbcb5ce68289a6dafa98204e64273b3
Keky <st.keky@gmail.com>
2016-11-02 20:13:32 +0100

    Exposed some headers

--- src/main/java/rs/acs/uns/sw/awt_test/configuration/CorsFilter.java
+++ src/main/java/rs/acs/uns/sw/awt_test/configuration/CorsFilter.java
@@ -27,7 +27,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, " + tokenHeader);
-
+        response.setHeader("Access-Control-Expose-Headers", "Link, X-Total-Count");
         chain.doFilter(req, res);
     }
 
