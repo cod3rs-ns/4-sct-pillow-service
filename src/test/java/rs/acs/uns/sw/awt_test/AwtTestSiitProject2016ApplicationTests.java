0f91d055cf8edc1cc036e3e9d325f69434e75853
Keky <st.keky@gmail.com>
2016-11-15 16:12:12 +0100

    RealEstate Controller test

--- src/test/java/rs/acs/uns/sw/awt_test/AwtTestSiitProject2016ApplicationTests.java
+++ src/test/java/rs/acs/uns/sw/awt_test/AwtTestSiitProject2016ApplicationTests.java
@@ -1,16 +1,13 @@
 package rs.acs.uns.sw.awt_test;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootApplication
 public class AwtTestSiitProject2016ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AwtTestSiitProject2016ApplicationTests.class, args);
+    }
 
 }
