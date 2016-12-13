820944fc35b811787bcc650f0c2b35fcffa30867
Dragutin Marjanovic <dmarjanovic94@gmail.com>
2016-12-13 16:13:50 +0100

    Quick fix

--- src/test/java/rs/acs/uns/sw/sct/constants/AnnouncementConstants.java
+++ src/test/java/rs/acs/uns/sw/sct/constants/AnnouncementConstants.java
@@ -64,7 +64,7 @@ public interface AnnouncementConstants {
 
     // Companies
     Long COMPANY_ID = 1L;
-    Integer COUNT_OF_COMPANY_ANN = 2;
+    Integer COUNT_OF_COMPANY_ANN = 3;
 
     // 1st page with 10 elements
     Pageable PAGEABLE = new PageRequest(0, 10, Sort.Direction.ASC, "dateAnnounced", "price");
