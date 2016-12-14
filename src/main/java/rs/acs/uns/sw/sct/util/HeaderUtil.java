b4da18d558085c349193c745da8ca336eb0d2885
Stefan Ristanović <st.keky@gmail.com>
2016-12-14 16:05:40 +0100

    Updated HeaderUtils. closes #43

--- src/main/java/rs/acs/uns/sw/sct/util/HeaderUtil.java
+++ src/main/java/rs/acs/uns/sw/sct/util/HeaderUtil.java
@@ -7,14 +7,19 @@ import org.springframework.http.HttpHeaders;
  */
 public final class HeaderUtil {
 
-    public static final String ANNOUNCEMENT = "announcement";
-    public static final String COMMENT = "comment";
-    public static final String COMPANY = "company";
-    public static final String MARK = "mark";
-    public static final String USER = "user";
-    public static final String REAL_ESTATE = "real_estate";
-    public static final String REPORT = "report";
+    // ========================== Header names ========================== //
+    private static final String SCT_HEADER_ALERT = "X-sct-alert";
+    private static final String SCT_HEADER_PARAMS = "X-sct-params";
+    private static final String SCT_HEADER_ERROR_KEY = "X-sct-error-key";
 
+    // ====================================== Common error messages ======================================= //
+    public static final String ERROR_MSG_UPDATE_DENIED = "You do not have permit to update this entity.";
+
+    // ============================================== Error codes ============================================== //
+    /**
+     * Code which receives user who cannot perform action because he is not the the <em>owner</em> of the entity
+     */
+    public static final Integer ERROR_CODE_NOT_OWNER = 1001;
 
     private HeaderUtil() {
     }
@@ -22,22 +27,23 @@ public final class HeaderUtil {
     /**
      * Creates Headers with specific alert entry.
      *
-     * @param message alert message - header value
-     * @param param   alert parameters - header value
+     * @param message explains reason for alert
+     * @param param  provides additional information about alert
      * @return HttpHeaders
      */
-    public static HttpHeaders createAlert(String message, String param) {
+    private static HttpHeaders createAlert(String message, String param) {
         HttpHeaders headers = new HttpHeaders();
-        headers.add("X-awt-test-alert", message);
-        headers.add("X-awt-test-params", param);
+        headers.add(SCT_HEADER_ALERT, message);
+        headers.add(SCT_HEADER_PARAMS, param);
         return headers;
     }
 
     /**
      * Creates Headers with specific Entity creation alert.
      *
-     * @param entityName name of the entity - header value
-     * @param param      other parameters - header value
+     * @param entityName name of the entity which instance is created and flushed to database
+     * @param param      other parameters which provides additional info,
+     *                   such as identifier assigned to the <em>newly created entity</em>
      * @return HttpHeaders
      */
     public static HttpHeaders createEntityCreationAlert(String entityName, String param) {
@@ -47,8 +53,9 @@ public final class HeaderUtil {
     /**
      * Creates Headers with specific Entity update alert.
      *
-     * @param entityName name of the entity - header value
-     * @param param      other parameters - header value
+     * @param entityName name of the entity which is updated
+     * @param param      other parameters which provides additional info,
+     *                   such as identifier of the <em>newly updated entity</em>
      * @return HttpHeaders
      */
     public static HttpHeaders createEntityUpdateAlert(String entityName, String param) {
@@ -58,8 +65,9 @@ public final class HeaderUtil {
     /**
      * Creates Headers with specific Entity deletion alert.
      *
-     * @param entityName name of the entity - header value
-     * @param param      other parameters - header value
+     * @param entityName name of the entity which is deleted
+     * @param param      other parameters which provides additional info,
+     *                   such as identifier of the <em>deleted entity</em>
      * @return HttpHeaders
      */
     public static HttpHeaders createEntityDeletionAlert(String entityName, String param) {
@@ -68,18 +76,19 @@ public final class HeaderUtil {
 
 
     /**
-     * Creates Headers with specific failure alers
+     * Creates Headers with specific failure alerts. This method is used to better explain reason for
+     * unsuccessful method invocation.
      *
-     * @param entityName     name of the entity - header value
-     * @param errorKey       error key
-     * @param defaultMessage default message to be shown - header value
+     * @param entityName     name of the entity on which action should have been performed
+     * @param errorKey       key from the above <em>ERROR CODES</em>
+     * @param defaultMessage default message to be shown - value from the above <em>ERROR MESSAGES</em>
      * @return HttpHeader
      */
-    public static HttpHeaders createFailureAlert(String entityName, String errorKey, String defaultMessage) {
+    public static HttpHeaders createFailureAlert(String entityName, Integer errorKey, String defaultMessage) {
         HttpHeaders headers = new HttpHeaders();
-        headers.add("X-sct-app-alert", defaultMessage);
-        headers.add("X-sct-app-params", entityName);
-        headers.add("X-sct-app-error-key", errorKey);
+        headers.add(SCT_HEADER_ALERT, defaultMessage);
+        headers.add(SCT_HEADER_PARAMS, entityName);
+        headers.add(SCT_HEADER_ERROR_KEY, errorKey.toString());
         return headers;
     }
 }
